ef2e6a1126741d5cd86893a6a7c87be800eccce2
HubertFer <humbertofernandezsosa@gmail.com>
2023-07-27 00:10:16 -0300

    H2 database, test profile, JPA

--- src/main/java/com/hubert/springboot3/resouces/UserResouce.java
+++ src/main/java/com/hubert/springboot3/resouces/UserResouce.java
@@ -2,12 +2,14 @@ package com.hubert.springboot3.resouces;
 
 import com.hubert.springboot3.entities.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value="/users")
 public class UserResouce {
+    @GetMapping
     public ResponseEntity<User> findAll(){
 
         User u = new User(1L, "Mary", "mary@gmail.com", "9999999999","12345");
