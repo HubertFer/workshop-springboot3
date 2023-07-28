7b4bdf39f8f77f834d5f992de9b72e77e4f51814
HubertFer <humbertofernandezsosa@gmail.com>
2023-07-27 21:09:51 -0300

    Service layer, component registration

--- src/main/java/com/hubert/springboot3/resouces/UserResouce.java
+++ src/main/java/com/hubert/springboot3/resouces/UserResouce.java
@@ -5,9 +5,11 @@ import com.hubert.springboot3.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -22,5 +24,11 @@ public class UserResouce {
 
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id ){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
